6c2b117b6c997df7c30bf89416b71f720e15c1b0
girish g <girishrathode.rathode@gmail.com>
2021-02-25 08:33:54 +0530

    25-02-2021

--- Parababnk/Examples/Class1.cs
+++ Parababnk/Examples/Class1.cs
@@ -18,10 +18,22 @@ namespace Parababnk.Examples
            driver = new ChromeDriver();
            driver.Navigate().GoToUrl("https://dev-online.dev.tsretail.co.za/landing");
             Thread.Sleep(10000);
-            driver.FindElement(By.XPath("/html/body/app-root/div/div/top-navigation/div/div/div/div[1]/div[2]/button-with-notification[2]")).Click();
+            /*driver.FindElement(By.XPath("/html/body/app-root/div/div/top-navigation/div/div/div/div[1]/div[2]/button-with-notification[2]")).Click();
             driver.FindElement(By.Id("qa-input-login-username")).SendKeys("Abc@xyz.com");
             driver.FindElement(By.Id("qa-input-login-password")).SendKeys("Abc@xyz.com");
-            driver.FindElement(By.Id("qa-sign-in-button")).Click();
+            driver.FindElement(By.Id("qa-sign-in-button")).Click();  */
+
+            driver.FindElement(By.XPath("/html/body/app-root/div/div/top-navigation/div/div/div/div[1]/div[2]/button-with-notification[1]")).Click();
+
+            driver.FindElement(By.XPath("/html/body/app-root/div/div/top-navigation/div/div/div/div[1]/div[2]/button-with-notification[1]")).Click();
+
+            driver.FindElement(By.Id("qa-first-name")).SendKeys("Abc@xyz.com");
+            driver.FindElement(By.Id("qa-surname")).SendKeys("Abc@xyz.com");
+            driver.FindElement(By.Id("qa-identification-input")).SendKeys("Abc@xyz.com");
+            driver.FindElement(By.Id("qa-password")).SendKeys("Abc@xyz.com");
+            driver.FindElement(By.Id("qa-confirm-password")).SendKeys("Abc@xyz.com");
+            driver.FindElement(By.Id("qa-next-on-personal-info")).Click();
+
         }
 
         
